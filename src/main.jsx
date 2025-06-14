427e878a40260587194ef3fbd6ee8ec995a64eeb
Mega Nugraha <monmega110@gmail.com>
2025-06-15 02:07:27 +0700

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
